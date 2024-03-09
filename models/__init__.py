bc4a68fddddae9a7b9935f2b7dafb21c2c92078b
bg-miles <piuschukwuebuka40@gmail>
2024-03-09 11:31:54 +0100

    main

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,3 @@ from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
-# __all__ = ["all","new","save", "reload","storage"]
--- models/base_model.py
+++ models/base_model.py
@@ -7,25 +7,29 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        setattr(
+                            self,
+                            key,
+                            datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
+                        )
                     else:
                         setattr(self, key, value)
 
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.today().strftime('%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%S.%f")
             storage.new(self)
 
     def save(self):
-        self.updated_at = datetime.datetime.today().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%S.%f")
         storage.save()
 
     def to_dict(self):
         dict_class = self.__dict__
-        dict_class['__class__'] = self.__class__.__name__
+        dict_class["__class__"] = self.__class__.__name__
         return dict_class
 
     def __str__(self):
-        return '[{}] {} {}'.format(__class__.__name__, self.id, self.__dict__)
+        return "[{}] {} {}".format(__class__.__name__, self.id, self.__dict__)
